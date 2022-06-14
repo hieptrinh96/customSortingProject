8c10d6b3964c697b9441b2bb85b8edd42c6ae9eb
Justin <mancussion@gmail.com>
2022-06-14 15:52:34 -0400

    ageSort and oddEvenSort done

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,9 +1,15 @@
 function ageSort(users) {
   // Your code here
+  return users.sort((a, b) => a.age - b.age)
 }
 
 function oddEvenSort(arr) {
   // Your code here
+  return arr.sort((a, b) => {
+    if (a % 2 === b % 2) return a - b
+    else if (a % 2 === 0) return 1
+    else if (a % 2 !== 0) return -1
+  })
 }
 
 function validAnagrams(s, t) {
